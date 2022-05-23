499abb7cb58cf770be97cdd85ad434f74038c8f6
wenjie <wjie@zju.edu.cn>
2022-05-23 19:03:18 +0800

    remove generated codes

--- examples/build.sh
+++ examples/build.sh
@@ -5,7 +5,5 @@ cd `dirname $0`
 PROTO_PATH="../interface/protobuf"
 OUT_PATH="./rpc_client"
 
-# conda activate ray
-
 mkdir -p $OUT_PATH
 python -m grpc_tools.protoc --proto_path=$PROTO_PATH --python_out=$OUT_PATH --grpc_python_out=$OUT_PATH $PROTO_PATH/*
--- examples/rpc_client/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os.path as osp
-
-sys.path.append(osp.dirname(osp.abspath(__file__)))
-
-from wart_types_pb2 import *
-from wart_worker_pb2 import *
-from wart_worker_pb2_grpc import WartWorkerStub
